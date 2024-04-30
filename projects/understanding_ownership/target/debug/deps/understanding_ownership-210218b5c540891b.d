99b9ee7da3b4e3423c9f088a86955fa2a825badc
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2024-04-30 08:42:49 +0100

    feat:deleted build

--- projects/understanding_ownership/target/debug/deps/understanding_ownership-210218b5c540891b.d
+++
@@ -1,5 +0,0 @@
-/Users/ibukunoluwaakintobi/Desktop/Defi Development/Rust-book/projects/understanding_ownership/target/debug/deps/libunderstanding_ownership-210218b5c540891b.rmeta: src/main.rs
-
-/Users/ibukunoluwaakintobi/Desktop/Defi Development/Rust-book/projects/understanding_ownership/target/debug/deps/understanding_ownership-210218b5c540891b.d: src/main.rs
-
-src/main.rs:
--- projects/understanding_ownership/target/debug/deps/understanding_ownership-e2b8c6d88372eb53.d
+++
@@ -1,5 +0,0 @@
-/Users/ibukunoluwaakintobi/Desktop/Defi Development/Rust-book/projects/understanding_ownership/target/debug/deps/libunderstanding_ownership-e2b8c6d88372eb53.rmeta: src/main.rs
-
-/Users/ibukunoluwaakintobi/Desktop/Defi Development/Rust-book/projects/understanding_ownership/target/debug/deps/understanding_ownership-e2b8c6d88372eb53.d: src/main.rs
-
-src/main.rs:
