cd80ef2fadd7e910c61b712f8aa0432370ce7af6
ibkisthere <ibukunoluwaakintobi@gmail.com>
2024-06-07 09:27:36 +0100

    chore:deleted target folder from Generic_Data_Types

--- projects/generic_types_traits_and_lifetimes/generic_data_types/target/debug/deps/generic_data_types-b9fdfc1acf7b0441.d
+++
@@ -1,5 +0,0 @@
-/Users/ibukunoluwaakintobi/Desktop/Defi Development/Rust-book/projects/generic_types_traits_and_lifetimes/generic_data_types/target/debug/deps/libgeneric_data_types-b9fdfc1acf7b0441.rmeta: src/main.rs
-
-/Users/ibukunoluwaakintobi/Desktop/Defi Development/Rust-book/projects/generic_types_traits_and_lifetimes/generic_data_types/target/debug/deps/generic_data_types-b9fdfc1acf7b0441.d: src/main.rs
-
-src/main.rs:
--- projects/generic_types_traits_and_lifetimes/generic_data_types/target/debug/deps/generic_data_types-fdcf662f736653d0.d
+++
@@ -1,5 +0,0 @@
-/Users/ibukunoluwaakintobi/Desktop/Defi Development/Rust-book/projects/generic_types_traits_and_lifetimes/generic_data_types/target/debug/deps/libgeneric_data_types-fdcf662f736653d0.rmeta: src/main.rs
-
-/Users/ibukunoluwaakintobi/Desktop/Defi Development/Rust-book/projects/generic_types_traits_and_lifetimes/generic_data_types/target/debug/deps/generic_data_types-fdcf662f736653d0.d: src/main.rs
-
-src/main.rs:
